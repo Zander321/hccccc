87277c3ceaf84f7f761dc6153749d2bfcbd2380b
huangchao <hc@123.com>
2022-09-07 11:24:04 +0800

    arr

---
+++ main.py
@@ -0,0 +1,27 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+def bubbleSort(arr):
+    n = len(arr)
+
+    # 遍历所有数组元素
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print("排序后的数组:")
+for i in range(len(arr)):
+    print("%d" % arr[i]),
